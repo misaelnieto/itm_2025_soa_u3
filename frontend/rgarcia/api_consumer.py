19dfcdc313f867777bfb9197d54d12c8355c8903
Noe Nieto <nnieto@noenieto.com>
2025-03-26 17:15:21 -0700

    Update frontend/rgarcia/api_consumer.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- frontend/rgarcia/api_consumer.py
+++ frontend/rgarcia/api_consumer.py
@@ -14,9 +14,7 @@ def get_recipes():
 def get_recipe(recipe_id : int):
     """"Get a recipe by its ID."""
     request = requests.get(f"http://127.0.0.1:8000/api/v1/rgarcia/recetas/receta?receta_id={recipe_id}", timeout=10)
-    if request.status_code == 200:
-        return json.loads(request.content)
-    return ''
+    return json.loads(request.content) if request.status_code == 200 else ''
 
 def post_recipe(data: dict):
     """Create a new recipe."""
